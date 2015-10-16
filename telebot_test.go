fea1835ff08a70e0e938da9f92dbc92cbd4fad43
aladine <dan.aladine@gmail.com>
2015-10-16 10:02:48 +0800

    Update telebot_test.go

--- telebot_test.go
+++ telebot_test.go
@@ -11,13 +11,13 @@ func TestBot(t *testing.T) {
 		t.Skip("skipping test in short mode.")
 	}
 
-	token := os.Getenv("TELEBOT_SECRET")
+	token := os.Getenv("TELEGRAM_SECRET ")
 	if token == "" {
 		fmt.Println("ERROR: " +
 			"In order to test telebot functionality, you need to set up " +
-			"TELEBOT_SECRET environmental variable, which represents an API " +
+			"TELEGRAM_SECRET environmental variable, which represents an API " +
 			"key to a Telegram bot.\n")
-		t.Fatal("Could't find TELEBOT_SECRET, aborting.")
+		t.Fatal("Could't find TELEGRAM_SECRET, aborting.")
 	}
 
 	_, err := NewBot(token)
