4103e705b00b8e36a88bee3f32b3e5c5a2ed9717
Ian Byrd <ian@byrd.im>
2015-12-05 11:40:55 +0200

    Fixing critical API issue; resolves #26

--- types.go
+++ types.go
@@ -120,12 +120,10 @@ type Video struct {
 
 // Contact object represents a contact to Telegram user
 type Contact struct {
+	UserID      int    `json:"user_id"`
 	PhoneNumber string `json:"phone_number"`
 	FirstName   string `json:"first_name"`
 	LastName    string `json:"last_name"`
-
-	// Contact's username in Telegram (might be empty).
-	Username string `json:"user_id"`
 }
 
 // Location object represents geographic position.
