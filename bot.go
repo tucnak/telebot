fc4d712ce02a40664472ccd56756a290eb8efc7e
aiden <ahmadrezazibaei@hotmail.com>
2016-10-09 23:46:33 +0330

    getChatMember added

--- bot.go
+++ bot.go
@@ -769,6 +769,37 @@ func (b *Bot) GetChatMembersCount(recipient Recipient) (int, error) {
 	return responseRecieved.Result, nil
 }
 
+// Use this method to get information about a member of a chat.
+//
+// Returns a ChatMember object on success.
+func (b *Bot) GetChatMember(recipient Recipient, user User) (ChatMember, error) {
+	params := map[string]string{
+		"chat_id": recipient.Destination(),
+		"user_id": user.Destination(),
+	}
+	responseJSON, err := sendCommand("getChatMember", b.Token, params)
+	if err != nil {
+		return ChatMember{}, err
+	}
+
+	var responseRecieved struct {
+		Ok          bool
+		Result      ChatMember
+		Description string `json:"description",omitempty`
+	}
+
+	err = json.Unmarshal(responseJSON, &responseRecieved)
+	if err != nil {
+		return ChatMember{}, err
+	}
+
+	if !responseRecieved.Ok {
+		return ChatMember{}, fmt.Errorf("telebot: getChatMember failure %s", responseRecieved.Description)
+	}
+
+	return responseRecieved.Result, nil
+}
+
 // GetFileDirectURL returns direct url for files using FileId which you can get from File object
 func (b *Bot) GetFileDirectURL(fileID string) (string, error) {
 	f, err := b.GetFile(fileID)
