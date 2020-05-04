5f4e2adc84a7378a5e226594066ed1205854b96f
Nikita Bikmaev <nikitabikmaev@gmail.com>
2020-05-04 23:55:23 +0300

    stickers: update sticker structure

--- stickers.go
+++ stickers.go
@@ -3,22 +3,31 @@ package telebot
 // Sticker object represents a WebP image, so-called sticker.
 type Sticker struct {
 	File
-
-	Width  int `json:"width"`
-	Height int `json:"height"`
-
-	Thumbnail    *Photo        `json:"thumb,omitempty"`
-	Emoji        string        `json:"emoji,omitempty"`
-	SetName      string        `json:"set_name,omitempty"`
-	MaskPosition *MaskPosition `json:"mask_position,omitempty"`
+	Width        int           `json:"width"`
+	Height       int           `json:"height"`
+	Animated     bool          `json:"is_animated"`
+	Thumbnail    *Photo        `json:"thumb"`
+	Emoji        string        `json:"emoji"`
+	Name         string        `json:"name"`
+	SetName      string        `json:"set_name"`
+	PNG          *File         `json:"png_sticker"`
+	TGS          *File         `json:"tgs_file"`
+	Emojis       string        `json:"emojis"`
+	MaskPosition *MaskPosition `json:"mask_position"`
 }
 
 // StickerSet represents a sticker set
 type StickerSet struct {
-	Name          string    `json:"name"`
-	Title         string    `json:"title"`
-	ContainsMasks bool      `json:"contains_masks"`
-	Stickers      []Sticker `json:"stickers"`
+	Name          string        `json:"name"`
+	Title         string        `json:"title"`
+	Animated      bool          `json:"is_animated"`
+	ContainsMasks bool          `json:"contains_masks"`
+	Stickers      []Sticker     `json:"stickers"`
+	Thumbnail     *Photo        `json:"thumb"`
+	PNG           *File         `json:"png_sticker"`
+	TGS           *File         `json:"tgs_file"`
+	Emojis        string        `json:"emojis"`
+	MaskPosition  *MaskPosition `json:"mask_position"`
 }
 
 // MaskPosition describes the position on faces where
