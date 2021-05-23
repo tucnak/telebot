b7575fce7041b962bf43d13ff5a5129ee1f22a57
Nikita <nikitadiscore@gmail.com>
2021-05-23 15:17:03 +0300

    webhooks: add ip_address and drop_pending_updates fields

--- webhook.go
+++ webhook.go
@@ -49,6 +49,9 @@ type Webhook struct {
 	ErrorUnixtime  int64  `json:"last_error_date"`
 	ErrorMessage   string `json:"last_error_message"`
 
+	IpAddress          string `json:"ip_address"`
+	DropPendingUpdates bool   `json:"drop_pending_updates"`
+
 	TLS      *WebhookTLS
 	Endpoint *WebhookEndpoint
 
@@ -87,6 +90,12 @@ func (h *Webhook) getParams() map[string]string {
 	if len(h.AllowedUpdates) > 0 {
 		data, _ := json.Marshal(h.AllowedUpdates)
 		params["allowed_updates"] = string(data)
+	}
+	if h.IpAddress != "" {
+		params["ip_address"] = h.IpAddress
+	}
+	if h.DropPendingUpdates {
+		params["drop_pending_updates"] = strconv.FormatBool(h.DropPendingUpdates)
 	}
 
 	if h.TLS != nil {
@@ -178,7 +187,13 @@ func (b *Bot) SetWebhook(w *Webhook) error {
 }
 
 // RemoveWebhook removes webhook integration.
-func (b *Bot) RemoveWebhook() error {
-	_, err := b.Raw("deleteWebhook", nil)
+func (b *Bot) RemoveWebhook(dropPending ...bool) error {
+	drop := false
+	if len(dropPending) > 0 {
+		drop = dropPending[0]
+	}
+	_, err := b.Raw("deleteWebhook", map[string]bool{
+		"drop_pending_updates": drop,
+	})
 	return err
 }
