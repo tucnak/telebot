79ace6bc54265ae83b696f5fd2f2427c07df0457
Demian <d@aletheia.icu>
2021-07-30 18:33:45 +0300

    middleware: implement auto callback respond

---
+++ middleware/respond.go
@@ -0,0 +1,14 @@
+package middleware
+
+import tele "gopkg.in/tucnak/telebot.v3"
+
+func Respond() tele.MiddlewareFunc {
+	return func(next tele.HandlerFunc) tele.HandlerFunc {
+		return func(c tele.Context) error {
+			if c.Callback() != nil {
+				defer c.Respond()
+			}
+			return next(c)
+		}
+	}
+}
