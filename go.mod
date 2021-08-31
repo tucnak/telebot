6ecbc6adaecbd6b3caccc0ecb88abf40767b79fd
Demian <d@aletheia.icu>
2021-08-31 15:15:53 +0300

    middleware: remove logrus as a dependency from Logger

--- go.mod
+++ go.mod
@@ -7,7 +7,6 @@ require (
 	github.com/goccy/go-yaml v1.9.1
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/pkg/errors v0.8.1
-	github.com/sirupsen/logrus v1.7.0
 	github.com/spf13/cast v1.3.1
 	github.com/stretchr/testify v1.5.1
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
--- middleware/logger.go
+++ middleware/logger.go
@@ -1,54 +1,21 @@
 package middleware
 
 import (
-	"github.com/sirupsen/logrus"
+	"encoding/json"
+	"log"
+
 	tele "gopkg.in/tucnak/telebot.v3"
 )
 
-type LoggerFieldsFunc = func(tele.Context) logrus.Fields
-
-func Logger(logger *logrus.Logger, fieldsFunc ...LoggerFieldsFunc) tele.MiddlewareFunc {
+func Logger(logger *log.Logger) tele.MiddlewareFunc {
 	if logger == nil {
-		logger = logrus.New()
+		logger = log.Default()
 	}
-
-	var f LoggerFieldsFunc
-	if len(fieldsFunc) > 0 && fieldsFunc[0] != nil {
-		f = fieldsFunc[0]
-	} else {
-		f = func(c tele.Context) logrus.Fields { return nil }
-	}
-
 	return func(next tele.HandlerFunc) tele.HandlerFunc {
 		return func(c tele.Context) error {
-			data := c.Data()
-			if clb := c.Callback(); clb != nil {
-				uniq := clb.Unique
-				if data == "" {
-					data = uniq
-				} else {
-					data = uniq + "|" + data
-				}
-			} else if data == "" {
-				data = c.Text()
-			}
-
-			logger.WithFields(f(c)).Info(data)
+			data, _ := json.MarshalIndent(c.Update(), "", "  ")
+			logger.Println(string(data))
 			return next(c)
 		}
 	}
-}
-
-func DefaultLogger() tele.MiddlewareFunc {
-	return Logger(logrus.New(), func(c tele.Context) logrus.Fields {
-		sender := c.Sender()
-		if sender == nil {
-			return nil
-		}
-
-		return logrus.Fields{
-			// Default set of fields
-			"sender": sender.Recipient(),
-		}
-	})
 }
