35526d3d53a66be09c0359778a6dda13b2fc3692
Demian <d@aletheia.icu>
2021-09-05 17:26:22 +0300

    middleware: make Logger argument optional

--- middleware/logger.go
+++ middleware/logger.go
@@ -7,14 +7,18 @@ import (
 	tele "gopkg.in/tucnak/telebot.v3"
 )
 
-func Logger(logger *log.Logger) tele.MiddlewareFunc {
-	if logger == nil {
-		logger = log.Default()
+func Logger(logger ...*log.Logger) tele.MiddlewareFunc {
+	var l *log.Logger
+	if len(logger) > 0 {
+		l = logger[0]
+	} else {
+		l = log.Default()
 	}
+
 	return func(next tele.HandlerFunc) tele.HandlerFunc {
 		return func(c tele.Context) error {
 			data, _ := json.MarshalIndent(c.Update(), "", "  ")
-			logger.Println(string(data))
+			l.Println(string(data))
 			return next(c)
 		}
 	}
