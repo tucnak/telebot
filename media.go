270e53bb90953d220a9128bb585b4f79eadd2a00
Vlad Lukyanov <vlad-lukyanov@users.noreply.github.com>
2023-11-20 01:19:34 +0500

    media: add missed CustomEmoji field to the Sticker (#579)
    Co-authored-by: Vladislav Lukianov <vladislav_lukianov@epam.com>

--- media.go
+++ media.go
@@ -280,15 +280,17 @@ func (v *VideoNote) MediaFile() *File {
 // Sticker object represents a WebP image, so-called sticker.
 type Sticker struct {
 	File
-	Width            int           `json:"width"`
-	Height           int           `json:"height"`
-	Animated         bool          `json:"is_animated"`
-	Video            bool          `json:"is_video"`
-	Thumbnail        *Photo        `json:"thumb"`
-	Emoji            string        `json:"emoji"`
-	SetName          string        `json:"set_name"`
-	MaskPosition     *MaskPosition `json:"mask_position"`
-	PremiumAnimation *File         `json:"premium_animation"`
+	Width            int            `json:"width"`
+	Height           int            `json:"height"`
+	Animated         bool           `json:"is_animated"`
+	Video            bool           `json:"is_video"`
+	Thumbnail        *Photo         `json:"thumb"`
+	Emoji            string         `json:"emoji"`
+	SetName          string         `json:"set_name"`
+	MaskPosition     *MaskPosition  `json:"mask_position"`
+	PremiumAnimation *File          `json:"premium_animation"`
+	Type             StickerSetType `json:"type"`
+	CustomEmoji      string         `json:"custom_emoji_id,omitempty"`
 }
 
 func (s *Sticker) MediaType() string {
