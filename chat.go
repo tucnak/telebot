06bbf9e69f565423852eff51613c0cc98fa19fcd
Demian <optigan@protonmail.com>
2023-11-20 13:34:51 +0100

    chat: add user_chat_id field

--- chat.go
+++ chat.go
@@ -190,6 +190,12 @@ type ChatJoinRequest struct {
 	// Sender is the user that sent the join request.
 	Sender *User `json:"from"`
 
+	// UserChatID is an ID of a private chat with the user
+	// who sent the join request. The bot can use this ID
+	// for 5 minutes to send messages until the join request
+	// is processed, assuming no other administrator contacted the user.
+	UserChatID int64 `json:"user_chat_id"`
+
 	// Unixtime, use ChatJoinRequest.Time() to get time.Time.
 	Unixtime int64 `json:"date"`
 
