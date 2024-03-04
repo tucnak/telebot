b00efaba98d5af92238be03a666bb2609042ab47
Nikita <nenten137@gmail.com>
2024-03-04 20:08:17 +0200

    media/test: refactor

--- media_test.go
+++ media_test.go
@@ -6,38 +6,39 @@ import (
 )
 
 func TestAlbumSetCaption(t *testing.T) {
-	var a Album
-	a = append(a, &Photo{Caption: "wrong_caption"})
-	a = append(a, &Photo{Caption: "t"})
-	a.SetCaption("correct_caption")
-	assert.Equal(t, "correct_caption", a[0].InputMedia().Caption)
-	assert.Equal(t, "t", a[1].InputMedia().Caption)
-
-	a = Album{}
-	a = append(a, &Animation{Caption: "wrong_caption"})
-	a = append(a, &Photo{Caption: "t"})
-	a.SetCaption("correct_caption")
-	assert.Equal(t, "correct_caption", a[0].InputMedia().Caption)
-	assert.Equal(t, "t", a[1].InputMedia().Caption)
-
-	a = Album{}
-	a = append(a, &Audio{Caption: "wrong_caption"})
-	a = append(a, &Photo{Caption: "t"})
-	a.SetCaption("correct_caption")
-	assert.Equal(t, "correct_caption", a[0].InputMedia().Caption)
-	assert.Equal(t, "t", a[1].InputMedia().Caption)
-
-	a = Album{}
-	a = append(a, &Document{Caption: "wrong_caption"})
-	a = append(a, &Photo{Caption: "t"})
-	a.SetCaption("correct_caption")
-	assert.Equal(t, "correct_caption", a[0].InputMedia().Caption)
-	assert.Equal(t, "t", a[1].InputMedia().Caption)
-
-	a = Album{}
-	a = append(a, &Video{Caption: "wrong_caption"})
-	a = append(a, &Photo{Caption: "t"})
-	a.SetCaption("correct_caption")
-	assert.Equal(t, "correct_caption", a[0].InputMedia().Caption)
-	assert.Equal(t, "t", a[1].InputMedia().Caption)
+	tests := []struct {
+		name  string
+		media Inputtable
+	}{
+		{
+			name:  "photo",
+			media: &Photo{Caption: "wrong_caption"},
+		},
+		{
+			name:  "animation",
+			media: &Animation{Caption: "wrong_caption"},
+		},
+		{
+			name:  "video",
+			media: &Video{Caption: "wrong_caption"},
+		},
+		{
+			name:  "audio",
+			media: &Audio{Caption: "wrong_caption"},
+		},
+		{
+			name:  "document",
+			media: &Document{Caption: "wrong_caption"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var a Album
+			a = append(a, tt.media)
+			a = append(a, &Photo{Caption: "random_caption"})
+			a.SetCaption("correct_caption")
+			assert.Equal(t, "correct_caption", a[0].InputMedia().Caption)
+			assert.Equal(t, "random_caption", a[1].InputMedia().Caption)
+		})
+	}
 }
